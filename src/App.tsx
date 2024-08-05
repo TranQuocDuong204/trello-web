6031d0a1ead3e366d084056c33bff5a3a6ea1ddc
TranQuocDuong204 <tranquocduong2004vta@gmail.com>
2024-08-05 21:08:49 +0700

    config_mui_theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { FC, useState } from "react";
-
+import { FC } from "react";
 import "./App.css";
-
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 const App: FC = () => {
   return (
-    <h1 className="text-6xl font-bold underline text-red-600">Hello world!</h1>
+    <h1 className="text-6xl font-bold underline text-red-600">
+      <Button variant="contained">Hello world</Button>
+      <Typography variant="h2" component="h2" color="text.secondary">
+        h1. Heading
+      </Typography>
+      Hello world!
+    </h1>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theme.tsx";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
