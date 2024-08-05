9db5cda75b7c20ed0a5077e68e3a61f90a6db232
TranQuocDuong204 <tranquocduong2004vta@gmail.com>
2024-08-05 15:11:31 +0700

    #1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC, useState } from "react";
+
+import "./App.css";
+
+const App: FC = () => {
+  return (
+    <h1 className="text-6xl font-bold underline text-red-600">Hello world!</h1>
+  );
+};
+
+export default App;
